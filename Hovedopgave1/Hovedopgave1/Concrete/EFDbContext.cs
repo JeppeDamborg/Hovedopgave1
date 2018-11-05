c6dc43b506145ff513179f722517e0af26ab9412
Tolga <Tolgasaj@Hotmail.com>
2018-11-05 11:06:33 +0100

    Lavet Concrete folder med EFDbContext LAvet connectionstring i Web.config

---
+++ Hovedopgave1/Hovedopgave1/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hovedopgave1.Models;
+using System.Data.Entity;
+
+namespace Hovedopgave1.Concrete
+{
+    public class EFDbContext
+    {
+
+    }
+}
