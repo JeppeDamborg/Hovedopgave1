f4098060a8649e2538d0694c056063d75fc77bd9
Tolga <Tolgasaj@Hotmail.com>
2018-11-07 11:53:53 +0100

    Har lavet required og errormessages for Student samt tekstbokse gemmer resterende data på siden efter opret ikke blev udført

--- Hovedopgave1/Hovedopgave1/Models/Students.cs
+++ Hovedopgave1/Hovedopgave1/Models/Students.cs
@@ -10,16 +10,27 @@ namespace Hovedopgave1.Models
     public class Students
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Du skal skrive dit navn")]
         public string Navn { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din adresse")]
         public string Adresse { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din Bopæl")]
         public string Bopæl { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din nationalitet")]
         public string Nationalitet { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din sprogkundskaber")]
         public string SprogKundskab { get; set; }
+        [Required(ErrorMessage = "Du skal skrive dit telefonnummer")]
         public string Telefon { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din mail")]
         public string Mail { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din uddannelse")]
         public string Uddannelse { get; set; }
+        [Required(ErrorMessage = "Du skal skrive Indtaste din periode")]
         public string Periode { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din nuværende semester")]
         public string Semester { get; set; }
+        [Required(ErrorMessage = "Du skal skrive din specialisering")]
         public string Specialisering { get; set; }
         public string Overbygning { get; set; }
         public string SemesterProjekt { get; set; }
