dbc589e3528a9b7f266031e5be9acb5c1ad53076
Tolga <Tolgasaj@Hotmail.com>
2018-11-08 10:28:08 +0100

    Required og Error sat på virksomhed

--- Hovedopgave1/Hovedopgave1/Models/Virksomhed.cs
+++ Hovedopgave1/Hovedopgave1/Models/Virksomhed.cs
@@ -10,22 +10,24 @@ namespace Hovedopgave1.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Du skal skrive navnet på virksomheden")]
         public string Navn { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive navnet på kontaktpersonen")]
         public string KontaktPerson { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive telefonnummeret ned")]
         public string Telefon { get; set; }
-
+        [Required(ErrorMessage = "Du skal have email med")]
         public string Mail { get; set; }
 
         public string VirksomhedsInfo { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive mulige opgaver")]
         public string MuligeOpgaver { get; set; }
-
+        [Required(ErrorMessage = "Du skal have profiler med")]
         public string Profiler { get; set; }
 
-        public string OpgaveLøsning { get; set; }
 
+        public string OpgaveLøsning { get; set; }
+        [Required(ErrorMessage = "Du skal have styrkeposition med")]
         public string Styrkeposition { get; set; }
     }
 }
