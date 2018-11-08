271a6442ec62c2547db0759eea6b7c06bdf10cb9
Tolga <Tolgasaj@Hotmail.com>
2018-11-08 13:48:57 +0100

    ErrorMessages for opret bruger virker nu

--- Hovedopgave1/Hovedopgave1/Models/Bruger.cs
+++ Hovedopgave1/Hovedopgave1/Models/Bruger.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hovedopgave1.Models
 {
@@ -9,10 +11,11 @@ namespace Hovedopgave1.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Du skal skrive brugernavnet")]
         public string Brugernavn { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive et password")]
         public string Password { get; set; }
-
+        [Required(ErrorMessage = "Du skal hvilken rettighed brugeren har")]
         public string Rettighed { get; set; }
 
     }
