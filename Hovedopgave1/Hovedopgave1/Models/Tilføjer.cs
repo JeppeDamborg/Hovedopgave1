284ba1871e942946a386ae6bb4bd22280a089a58
Tolga <Tolgasaj@Hotmail.com>
2018-11-19 13:36:22 +0100

    Her nu sat errormessages på tilføjer, brugeren får nu besked når det prøver at oprette en tilføjer uden en felt er blevet fyldt ud

--- Hovedopgave1/Hovedopgave1/Models/Tilføjer.cs
+++ Hovedopgave1/Hovedopgave1/Models/Tilføjer.cs
@@ -12,26 +12,28 @@ namespace Hovedopgave1.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Du skal skrive dit navn")]
         public string Navn { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive din adresse")]
         public string Adresse { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive dit telefonnummer")]
         public string Telefon { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive din mail")]
+        [EmailAddress(ErrorMessage = "Ugyldigt Email Addresse")]
         public string Mail { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive din uddannelse")]
         public string Uddannelse { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive dine erhvervserfaringer")]
         public string Erhvervserfaring { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive dit job ønske")]
         public string JobØnske { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive dine faglige kompetencer")]
         public string FagligeKompetencer { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive dine personlige kompetencer")]
         public string PersonligeKompetencer { get; set; }
 
         public string GenereltInfo { get; set; }
-
+        [Required(ErrorMessage = "Du skal skrive din uddannelse")]
         public bool ØnskerAtFlytte { get; set; }
 
 
