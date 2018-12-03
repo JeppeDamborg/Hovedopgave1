0c3dd79a9d5a99e66de9280af7750941e47a5c0a
Jeppe Damborg <jeppe_09@hotmail.dk>
2018-12-03 11:09:11 +0100

    Har lavet controller til Login med indhold

---
+++ Hovedopgave1/Hovedopgave1/Controllers/BrugerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hovedopgave1.Abstract;
+using Hovedopgave1.Models;
+
+namespace Hovedopgave1.Controllers
+{
+    public class BrugerController : Controller
+    {
+        IAuthProvider authProvider;
+
+        public BrugerController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(Bruger bruger, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if(authProvider.Authenticate(bruger.Brugernavn, bruger.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Forside", "Home"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Forkert brugernavn eller password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
