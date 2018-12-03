0608d847ae1f9434ff8b0abfb5bc729077538a56
Tolga <Tolgasaj@Hotmail.com>
2018-12-03 10:52:12 +0100

    Lavet Interface IAuthProvider

---
+++ Hovedopgave1/Hovedopgave1/Abstract/IAuthProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hovedopgave1.Abstract
+{
+    interface IAuthProvider
+    {
+        bool Authenticate(string username, string password);
+    }
+}
